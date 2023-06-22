f2f17f5aa58c95c75af67b9f09e954196216dc24
Naufal Irsyad Ihsan <naufalirsd.ppqita@gmail.com>
2023-06-22 12:52:01 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Navigasi from "./components/Navigation";
+import Headers from "./components/Headers";
+import About from "./components/About";
+import Unit from "./components/Unit";
+import Profile from "./components/Profil-galeri";
+import Footer from "./components/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Navigasi />
+            <Headers />
+            <About />
+            <Unit />
+            <Profile />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
